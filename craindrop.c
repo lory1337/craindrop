d160d79bbbfbd91ca78d75a6bdff7eaf3fa78387
Lorenzo <lorenzo.03x@libero.it>
2024-09-12 00:16:43 +0200

    Add files via upload

--- craindrop.c
+++ craindrop.c
@@ -1,10 +1,66 @@
 #include <ncurses.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
+
+/*
+  Available colors:
+  COLOR_BLACK
+  COLOR_RED
+  COLOR_GREEN
+  COLOR_YELLOW
+  COLOR_BLUE
+  COLOR_MAGENTA
+  COLOR_CYAN
+  COLOR_WHITE
+*/
+
+/*
+creates the matrix then print the content
+*/
+
 
 int main(int argc, char * argv[]){
-  initscr();
+  setlocale(LC_ALL, "");
+  initscr(); //init the screen
+  start_color(); //init the colors
+  curs_set(0); //hide cursor
+  if(has_colors() == FALSE) {
+    endwin();
+    printf("Your terminal does not support color\n");
+    exit(1);
+  }
+  init_pair(1, COLOR_BLUE, COLOR_BLACK);
+  init_pair(2, COLOR_CYAN, COLOR_BLUE);
+  init_pair(3, COLOR_BLACK, COLOR_WHITE);
+  init_pair(4, COLOR_RED, COLOR_MAGENTA);
+  //setlocale(LC_ALL, ""); //set local charcaters
+  //attron(COLOR_PAIR(COLOR_BLUE));
+  attron(COLOR_PAIR(1));/*
+  printw("lines= %d columns= %d\n", LINES, COLS); //dimension of console
+  printw("a");
+  printw("b");*/
+  int i = 0;
+  //wchar_t unichar = 0x2588;
+  for(int k=2;k<100;k++){
+    clear();
+    i = 0;
+    while(i<LINES*COLS){
+      if(i%(k%5+2)!=0 && i%19==0)
+        printw("!"/*, unichar*/);
+      else
+        printw(" "/*, unichar*/);
+      ++i;
+    }
+    napms(100*10); //wait millisecond
+    refresh();
+  }
+  attroff(COLOR_PAIR(1));
   //printw(); //use instead of printf()
   //getch(); //program wait for input
-  //refresh(); //show the results
+  refresh(); //show the results
+  getch(); //before exit
   endwin(); //IMPORTANT: prevents crash
   return 0;
 }
